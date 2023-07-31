a12ebc9c81652a2cde449a6d4259e2ef8735347d
kiwy36 <kevinwittner92@gmail.com>
2023-07-31 02:29:42 -0300

    fallos

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,24 @@ import 'react-app-polyfill/stable';
 import { BrowserRouter } from 'react-router-dom';
 
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyAlgztwIEMrmv59PNf5xEfDhILZzTQrakE",
+  authDomain: "indumentaria-e6763.firebaseapp.com",
+  databaseURL: "https://indumentaria-e6763-default-rtdb.firebaseio.com",
+  projectId: "indumentaria-e6763",
+  storageBucket: "indumentaria-e6763.appspot.com",
+  messagingSenderId: "629294298408",
+  appId: "1:629294298408:web:70db1e4e46386d09be4c1d",
+  measurementId: "G-SP3EPGXQ65"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
