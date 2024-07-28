09918a651d4fa45b9571158991cbe385f634aee5
kiwy36 <kevinwittner92@gmail.com>
2024-07-28 20:33:08 -0300

    minuuptadate

--- src/components/CustomNavbar/CustomNavbar.jsx
+++ src/components/CustomNavbar/CustomNavbar.jsx
@@ -1,24 +1,33 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './CustomNavbar.css';
 
 function CustomNavbar() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavClick = () => {
+    setExpanded(false);
+  };
+
   return (
     <>
-      <Navbar expand="lg" bg="dark" variant="dark" sticky="top" className='barra-de-navegacion'>
+      <Navbar expand="lg" bg="dark" variant="dark" sticky="top" className='barra-de-navegacion'expanded={expanded}>
         <Container fluid className='container'>
-          <Navbar.Brand href='#inicio' id="banner-link" onClick={() => {}}><h1 className="logo">Wittner Kevin</h1></Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbar-nav" />
+          <Navbar.Brand href='#inicio' id="banner-link" onClick={() => setExpanded(false)}>
+            <h1 className="logo">Wittner Kevin</h1>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-nav" onClick={() => setExpanded(!expanded)} />
           <Navbar.Collapse id="navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link id="mydate-link" onClick={() => {}}>
+              <Nav.Link id="mydate-link"  onClick={handleNavClick}>
                 <li className="link">Sobre mi</li>
               </Nav.Link>
-              <Nav.Link id="curriculum-link" onClick={() => {}}>
+              <Nav.Link id="curriculum-link" onClick={handleNavClick}>
                 <li className="link">Skills</li>
               </Nav.Link>
-              <Nav.Link id="proyec-link" onClick={() => {}}>
+              <Nav.Link id="proyec-link" onClick={handleNavClick}>
                 <li className="link">Proyectos</li>
               </Nav.Link>
               {/* <Nav.Link id="contacto-link" onClick={() => {}}>
