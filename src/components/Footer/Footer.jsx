ec646a53620ff149e42f9db8f15bc780e9d83dfb
kiwy36 <kevinwittner92@gmail.com>
2024-07-31 01:01:31 -0300

    memorama actualizado

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,14 +2,12 @@
 import "./Footer.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
-import { faHouse, faAnglesUp } from "@fortawesome/free-solid-svg-icons";
+import { faHouse} from "@fortawesome/free-solid-svg-icons";
 
 
 const Footer = () => {
     return <>
-    <footer>
-        <a className="arriba"href="#inicio" id="mydate-link" onClick={() => {}}><FontAwesomeIcon icon={faAnglesUp} size="1x" className="icono"  /></a>
-        <div className='redes'>
+    <footer><div className='redes'>
             <a href="/" className="nav-link-custom" rel="noreferrer"><FontAwesomeIcon icon={faHouse} size="1x" className="icono" /></a>
             <a href="https://github.com/kiwy36" target="_blank" className="nav-link-custom" rel="noreferrer"><FontAwesomeIcon icon={faGithub} size="1x" className="icono" /></a>
             <a href="https://www.instagram.com/kiwy_92/" target="_blank"className="nav-link-custom" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} size="1x" className="icono" /></a>
